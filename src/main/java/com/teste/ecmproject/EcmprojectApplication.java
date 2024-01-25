963c5ace6e1a47e61f91ab833d7260c4c04c6352
LM-Paulo <plucasmoraisn@gmail.com>
2024-01-25 18:32:43 -0300

    Initial commit

---
+++ src/main/java/com/teste/ecmproject/EcmprojectApplication.java
@@ -0,0 +1,13 @@
+package com.teste.ecmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcmprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teste/ecmproject/EcmprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.ecmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
