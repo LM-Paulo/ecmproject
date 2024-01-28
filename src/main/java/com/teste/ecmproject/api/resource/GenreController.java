0d074d1253c9ef8451a0f26b61245d623e3bc255
LM-Paulo <plucasmoraisn@gmail.com>
2024-01-28 18:07:34 -0300

    ECMPROJECT#07 Implementando controllers

---
+++ src/main/java/com/teste/ecmproject/api/resource/GenreController.java
@@ -0,0 +1,49 @@
+package com.teste.ecmproject.api.resource;
+
+import com.teste.ecmproject.api.dto.GenreDto;
+import com.teste.ecmproject.model.entity.GenreEntity;
+import com.teste.ecmproject.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/genres")
+public class GenreController {
+
+    private final GenreService genreService;
+
+    @Autowired
+    public GenreController(GenreService genreService) {
+        this.genreService = genreService;
+    }
+
+    @PostMapping
+    public void createGenre(@RequestBody GenreDto genreDto) {
+        genreService.createGenre(genreDto);
+    }
+
+    @PutMapping("/{id}")
+    public void updateGenre(@PathVariable UUID id, @RequestParam String genreName, @RequestParam Object newValue) {
+        genreService.update(id, genreName, newValue);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGenre(@PathVariable UUID id) {
+        genreService.delete(id);
+    }
+
+    @GetMapping
+    public Page<GenreEntity> listAllGenres(
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "properties", defaultValue = "name") String properties,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size) {
+
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        return genreService.listAllGenre(sortDirection, properties, page, size);
+    }
+}
---
+++ src/main/java/com/teste/ecmproject/api/resource/MovieController.java
@@ -0,0 +1,73 @@
+package com.teste.ecmproject.api.resource;
+
+import com.teste.ecmproject.api.dto.MovieDto;
+import com.teste.ecmproject.model.entity.MovieEntity;
+import com.teste.ecmproject.service.impl.MovieServiceImpl;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/movie")
+public class MovieController {
+
+    private final MovieServiceImpl movieService;
+    @Autowired
+    public MovieController(MovieServiceImpl movieService){
+        this.movieService = movieService;
+    }
+    @PostMapping("/create")
+    public ResponseEntity<?> createMovie(@RequestBody MovieDto dto){
+        movieService.createMovie(dto);
+        return ResponseEntity.ok("Successfully created movie");
+    }
+
+    @PutMapping
+    @Transactional
+    public void updateMovie(@RequestBody MovieDto dto){
+        movieService.update(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable UUID id) {
+        movieService.delete(id);
+    }
+
+    @GetMapping("/search")
+    public List<MovieEntity> findMoviesByName(@RequestParam("name") String name) {
+        return movieService.findMoviesByName(name);
+    }
+
+    @GetMapping("/searchByGenre")
+    public List<MovieEntity> findMoviesByGenre(@RequestParam("genre") String genre) {
+        return movieService.findMovieByGenre(genre);
+    }
+
+    @GetMapping("/allMovies")
+    public Page<MovieEntity> listAllMovies(
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "properties", defaultValue = "name") String properties,
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size) {
+
+        Sort.Direction sortDirection = Sort.Direction.fromString(direction);
+        return movieService.listAllMovies(sortDirection, properties, page, size);
+    }
+
+    @GetMapping("/unavailable")
+    public List<MovieEntity> findAllUnavailableMovies() {
+        return movieService.findAllUnavailableMovies();
+    }
+
+
+
+
+
+
+}
